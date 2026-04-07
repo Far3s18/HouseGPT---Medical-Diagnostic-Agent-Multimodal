804fa3caa297326dc040bc70c13205d8beef547a
Fadi Fares <faresfadi0412@gmail.com>
2026-04-07 15:43:02 +0100

    feat(chains): build chains

---
+++ src/house_gpt/agent/chains/__init__.py
@@ -0,0 +1,3 @@
+from .chains import get_character_response_chain, get_router_chain
+
+__all__ = ["get_router_chain", "get_character_response_chain"]
---
+++ src/house_gpt/agent/chains/chains.py
@@ -0,0 +1,35 @@
+from house_gpt.agent.helpers.model_factory import get_small_model, get_large_model
+from house_gpt.agent.helpers.formatter import AsteriskRemovalParser
+from house_gpt.states.response import RouterResponse
+from house_gpt.agent.prompts import ROUTER_PROMPT, CHARACTER_CARD_PROMPT
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+def get_router_chain():
+    model = get_small_model(temperature=0.2).with_structured_output(RouterResponse)
+
+    prompt =ChatPromptTemplate.from_messages([
+        ('system', ROUTER_PROMPT),
+        ('placeholder', '{messages}')
+    ])
+
+    chain = prompt | model
+
+    return chain
+
+
+def get_character_response_chain(summary: str = ""):
+    model = get_small_model()
+
+    system_message = CHARACTER_CARD_PROMPT
+
+    if summary:
+        system_message += f"\n\nSummary of conversation earlier between Dr House and the user: {summary}"
+
+    prompt = ChatPromptTemplate.from_messages([
+        ('system', system_message),
+        ('placeholder', '{messages}')
+    ])
+
+    chain = prompt | model | AsteriskRemovalParser()
+
+    return chain
