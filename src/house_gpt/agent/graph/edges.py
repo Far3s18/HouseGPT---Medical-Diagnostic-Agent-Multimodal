4610fddd953984c0fbfd92ca1636450c4ae4966a
Fadi Fares <faresfadi0412@gmail.com>
2026-04-07 15:37:21 +0100

    feat(graph): build initial langgraph workflow with routing

---
+++ src/house_gpt/agent/graph/edges.py
@@ -0,0 +1,20 @@
+from house_gpt.states.house import AIHouseState
+from house_gpt.core.settings import settings
+from typing import Literal
+from langgraph.graph import END
+
+
+def should_summarize_conversation(state: AIHouseState,) -> Literal["summarize_conversation_node", "__end__"]:
+    messages = state["messages"]
+
+    if len(messages) > settings.TOTAL_MESSAGES_SUMMARY_TRIGGER:
+        return "summarize_conversation_node"
+
+    return END
+
+def select_workflow(state: AIHouseState) -> Literal["conversation_node"]:
+    workflow = state["workflow"]
+    if workflow == "conversation":
+        return "conversation_node"
+    else:
+        return END
